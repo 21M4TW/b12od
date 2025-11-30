651ca0915b394b489c51b97489f082765446f509
21M4TW <21M4TW@proton.me>
2025-11-29 21:53:34 -0500

    -Fixing out of range char values in bech32_decode.h

--- include/b12od/bech32_decode.h
+++ include/b12od/bech32_decode.h
@@ -7,7 +7,7 @@
 
 enum eBech32DecodeErrors{BECH32_OK=0, BECH32_MEMORY_ALLOC_ISSUE=INT32_MIN, BECH32_NO_PREAMBLE_DIVIDER=1, BECH32_UPPER_LOWER_MIX=2, BECH32_INVALID_CHARACTER=3};
 
-enum eBech32SpecialTypes{BECH32_SPACE=254, BECH32_ERROR=255};
+enum eBech32SpecialTypes{BECH32_SPACE=126, BECH32_ERROR=127};
 
 int bech32_decode(const char* bech32_string, char** prefix, uint8_t** data, size_t* len);
 
