82b578ad8f2b1108e28ec7d5e5d5dc4047edd9b5
21M4TW <21M4TW@proton.me>
2025-12-01 10:44:28 -0500

    -Adding Rust test

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -36,3 +36,16 @@ impl Drop for OfferDecoder {
         unsafe { bolt12_offer_json_delete(self.ptr) };
     }
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*; // bring the function into scope
+
+  #[test]
+  fn test_offer_decode() {
+    let offer_decoder = OfferDecoder::new();
+    let result = offer_decoder.decode("lno1pgx9getnwss8vetrw3hhyucsl5qj5qeyv5l2cs6y3qqzesrth7mlzrlp3xg7xhulusczm04x6g6nms9trspqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqqsqqqqqqqqqqqqqqqqqqqqqqqqqqpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqsqpqg3zyg3zyg3zygpqqqqzqqqqgqqxqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqqgqqqqqqqqqqqqqqqqqqqqqqqqqqqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgpqyqszqgqqsg3zyg3zyg3zygtzzqhwcuj966ma9n9nqwqtl032xeyv6755yeflt235pmww58egx6rxry");
+    let expected = "{\"offer_description\":\"Test vectors\",\"offer_paths\":[{\"first_node_id\":\"0324653eac434488002cc06bbfb7f10fe18991e35f9fe4302dbea6d2353dc0ab1c\",\"first_path_key\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"path\":[{\"blinded_node_id\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"encrypted_recipient_data\":\"00000000000000000000000000000000\"},{\"blinded_node_id\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"encrypted_recipient_data\":\"1111111111111111\"}]},{\"first_scid\":\"1x2x3\",\"first_scid_dir\":1,\"first_path_key\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"path\":[{\"blinded_node_id\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"encrypted_recipient_data\":\"00000000000000000000000000000000\"},{\"blinded_node_id\":\"020202020202020202020202020202020202020202020202020202020202020202\",\"encrypted_recipient_data\":\"2222222222222222\"}]}],\"offer_issuer_id\":\"02eec7245d6b7d2ccb30380bfbe2a3648cd7a942653f5aa340edcea1f283686619\"}";
+    assert_eq!(result, expected);
+  }
+}
