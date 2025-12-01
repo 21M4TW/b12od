1832484cb52b2e66bd8af5c3d587541a6f489232
21M4TW <21M4TW@proton.me>
2025-11-30 23:31:32 -0500

    -bebuf: MacOS does not define de endian macros as other OSes in endian.h. Trying to address the issue

--- include/b12od/bebuf.h
+++ include/b12od/bebuf.h
@@ -2,7 +2,28 @@
 #define _BEBUF_
 
 #include <stdint.h>
+
+#ifndef __APPLE__
 #include <endian.h>
+
+#else
+#include <libkern/OSByteOrder.h>
+
+#define htobe16(x) OSSwapHostToBigInt16(x)
+#define htole16(x) OSSwapHostToLittleInt16(x)
+#define be16toh(x) OSSwapBigToHostInt16(x)
+#define le16toh(x) OSSwapLittleToHostInt16(x)
+
+#define htobe32(x) OSSwapHostToBigInt32(x)
+#define htole32(x) OSSwapHostToLittleInt32(x)
+#define be32toh(x) OSSwapBigToHostInt32(x)
+#define le32toh(x) OSSwapLittleToHostInt32(x)
+
+#define htobe64(x) OSSwapHostToBigInt64(x)
+#define htole64(x) OSSwapHostToLittleInt64(x)
+#define be64toh(x) OSSwapBigToHostInt64(x)
+#define le64toh(x) OSSwapLittleToHostInt64(x)
+#endif
 
 static inline uint16_t bebuf16toh(uint8_t const* const buf){return be16toh(*(uint16_t*)(buf));}
 static inline uint32_t bebuf32toh(uint8_t const* const buf){return be32toh(*(uint32_t*)(buf));}
