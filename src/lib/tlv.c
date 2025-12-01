e3c4a9b748f77c7d9370987c6f9de0ed7fc253f4
21M4TW <21M4TW@proton.me>
2025-12-01 00:50:28 -0500

    read_tlv: Fixing the verification of the TLV length read.

--- src/lib/tlv.c
+++ src/lib/tlv.c
@@ -5,11 +5,14 @@
 
 size_t read_tlv(byte const* buf, size_t maxlength, struct tlv_record* tlv)
 {
-  if(maxlength < 2) return 0;
-  byte ret = read_bigsize(buf, maxlength, &tlv->type);
+  byte ret, ret2;
 
-  if(!ret) return 0;
-  ret += read_bigsize(buf + ret, maxlength - ret, &tlv->length);
+  if(maxlength < 2) return 0;
+  ret = read_bigsize(buf, maxlength, &tlv->type);
+  ret2 = read_bigsize(buf + ret, maxlength - ret, &tlv->length);
+
+  if(!ret || !ret2) return 0;
+  ret += ret2;
 
   if(maxlength-ret < tlv->length) return 0;
   tlv->value = (byte*)malloc(tlv->length);
